61c5e8a11b0b1f25c5647467eb8c17cfc851e58f
avan-es <bagratavanesian@yandex.ru>
2023-09-24 15:23:14 +0300

    fix: документирующие комментарии

--- src/main/java/ru/handh/school/spb/Creatures/Creature.java
+++ src/main/java/ru/handh/school/spb/Creatures/Creature.java
@@ -1,5 +1,10 @@
 package ru.handh.school.spb.Creatures;
 
+/**
+ * Creature - родительский класс с ключевыми полями.
+ * Использование: для создания разновидностей существ.
+ * */
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +19,30 @@ public class Creature {
 
     protected String name;
 
+    /**
+     * Атака существа. Допустимые значения: [1; 30].
+     * */
     protected int attack;
 
+    /**
+     * Защита существа. Допустимые значения: [1; 30].
+     * */
     protected int defense;
 
+    /**
+     * Здоровье существа. Допустимые значения: [0; N). N - максимальное значение диапазона здоровья.
+     * По умолчанию равно 100.
+     * */
     protected int health;
 
+    /**
+     * Урон - максимальный урон, который может наносить существо.
+     * Допустимое значение (M; N). Значение по умолчанию (5; 15).
+     * */
     protected int damage;
 
+    /** Модификатор атаки существа. Равен разности Атаки атакующего и Защиты защищающегося плюс 1.
+     * */
     protected int attackPower;
 
 }
