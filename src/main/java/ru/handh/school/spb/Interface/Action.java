31efee5a9bff25457f828da0c62362e7aef3e5a6
avan-es <bagratavanesian@yandex.ru>
2023-09-24 16:15:12 +0300

    fix: рефакторинг; документирующие комментарии.

--- src/main/java/ru/handh/school/spb/Interface/Action.java
+++ src/main/java/ru/handh/school/spb/Interface/Action.java
@@ -1,5 +1,9 @@
 package ru.handh.school.spb.Interface;
 
+/**
+ * Интерфейс с основными методами для реализации.
+ * */
+
 import ru.handh.school.spb.Creatures.Creature;
 import ru.handh.school.spb.Creatures.Monster;
 import ru.handh.school.spb.Creatures.User;
@@ -8,23 +12,63 @@ import java.util.List;
 
 public interface Action {
 
-    void fight ();
-
-    boolean attack (Creature attack, Creature defend);
-
-    void checkHealth (User user);
-
-    void finish(Creature monster, Creature user);
-    Monster createMonster ();
-
-    User createUser ();
-
+    /**
+     * Метод создает объект класса Creature.
+     */
     Creature createCreature(int id, String name);
 
-    //Вычислить модификатор атаки (N)
-    int calculateAttackPower (Creature attack, Creature defend);
+    /**
+     * Метод генерирует псевдослучайные значения для характеристик Creature.
+     * Атака (attack) [1; 30];
+     * Защита (defense) [1; 30];
+     * Здоровье (health) [1; 100];
+     * Урон (damage) (5; 15).
+     * */
+    List<Integer> generateParam();
 
-    List<Integer> generateParam ();
+    /**
+     * Метод создаёт объект класса Monster.
+     * */
+    Monster createMonster();
+
+    /**
+     * Метод создаёт объект класса User.
+     * */
+    User createUser();
+
+    /**
+     * Метод вычисляет и устанавливает Модификатор атаки по формуле:
+     * Атака атакующего - Защита защищающегося + 1;
+     * */
+    void setAttackPower(Creature creatureOne, Creature creatureTwo);
+
+    /**
+     * Метод борьбы между существами.
+     * */
+    void fight ();
+
+    /**
+     * Метод атаки одного существа - другого.
+     * */
+    boolean attack(Creature attack, Creature defend);
+
+    /**
+     * Проверка уровня здоровья Пользователя. Позволяет предложить аптечку.
+     */
+    void checkHealth(User user);
+
+    /**
+     * Метод обработки результатов боя.
+     */
+    void finish(Creature monster, Creature user);
+
+
+
+
+
+
+
+
 
 
 }
